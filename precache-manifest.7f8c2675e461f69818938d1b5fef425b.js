c200e187691acb10ee61e776be8d12a549c7a731
Shant Parseghian <shant@Shant-Parseghian-MacBook-Pro.local>
2020-01-28 01:46:44 -0800

    Updates

---
+++ precache-manifest.7f8c2675e461f69818938d1b5fef425b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbef483688fb79a417b5",
+    "url": "/stripless-wax/static/js/runtime~main.dbef4836.js"
+  },
+  {
+    "revision": "b43bb5dc652da770b71a",
+    "url": "/stripless-wax/static/js/main.b43bb5dc.chunk.js"
+  },
+  {
+    "revision": "30d8e3e5c44621963571",
+    "url": "/stripless-wax/static/js/1.30d8e3e5.chunk.js"
+  },
+  {
+    "revision": "b43bb5dc652da770b71a",
+    "url": "/stripless-wax/static/css/main.4f597b84.chunk.css"
+  },
+  {
+    "revision": "9f01dbd75632052f4f2a35ab7cf7c81d",
+    "url": "/stripless-wax/index.html"
+  }
+];
